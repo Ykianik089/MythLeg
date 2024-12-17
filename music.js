923a574fa61972360bd9b934380ca50c9eb88381
Marinosbo <143496901+Marinosbo@users.noreply.github.com>
2024-12-18 00:30:42 +0300

    Update music.js

--- music.js
+++ music.js
@@ -1,23 +1,4 @@
 // Обработчик клика на всей странице
-document.addEventListener("click", function(event) { // event добавлен сюда
+document.addEventListener("click", function() {
     window.location.href = "Путешествие.html";
-});
-
-
-const music = document.getElementById('backgroundMusic');
-const musicToggle = document.getElementById('musicToggle');
-
-musicToggle.addEventListener('click', (event) => { // event добавлен сюда
-    event.stopPropagation();
-    const music = document.getElementById('backgroundMusic');
-    if (music.paused) {
-        music.muted = false;
-        music.play();
-        musicToggle.classList.remove('off');
-        // musicToggle.textContent = 'Отключить музыку';
-    } else {
-        music.pause();
-        // musicToggle.textContent = 'Включить музыку';
-        musicToggle.classList.add('off'); // Добавляем класс "off" — показываем "выключено"
-    }
 });
